935a290a92af093d148a6fa8417d9057a780e95b
Jose Pefaur <jose.pefaur@gmail.com>
2018-11-09 16:24:30 -0500

    make SingleAppManifestManager accessible by adding public static methods (#13)

--- src/main/java/com/liveramp/captain/manifest_manager/SingleAppManifestManager.java
+++ src/main/java/com/liveramp/captain/manifest_manager/SingleAppManifestManager.java
@@ -1,13 +1,12 @@
 package com.liveramp.captain.manifest_manager;
 
-import java.util.Set;
-
 import com.google.common.collect.Sets;
-
 import com.liveramp.captain.app_type.CaptainAppType;
 import com.liveramp.captain.manifest.Manifest;
 import com.liveramp.captain.manifest.ManifestFactory;
 import com.liveramp.captain.manifest.WrapperManifestFactoryImpl;
+
+import java.util.Set;
 
 public class SingleAppManifestManager implements ManifestManager {
   private final String SINGLE_APP_TYPE_PLACEHOLDER_KEY = "ALL";
@@ -15,6 +14,14 @@ public class SingleAppManifestManager implements ManifestManager {
 
   SingleAppManifestManager(Manifest manifest) {
     this(new WrapperManifestFactoryImpl(manifest));
+  }
+
+  public static SingleAppManifestManager ofManifests(Manifest manifest) {
+    return new SingleAppManifestManager(new WrapperManifestFactoryImpl(manifest));
+  }
+
+  public static SingleAppManifestManager ofManifestFactories(ManifestFactory manifestFactory) {
+    return new SingleAppManifestManager(manifestFactory);
   }
 
   SingleAppManifestManager(ManifestFactory manifestFactory) {
